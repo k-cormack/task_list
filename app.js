711495895cbd8f0789e02393858311b4c3639f2a
Keith Cormack <kcormack@definitivelogic.com>
2019-03-28 12:39:46 -0600

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// for(let i = 0; i <= 10; i++) {
+//   if(i === 8){
+//     console.log(`My favorite number is ${i}`);
+//     continue;
+//   }
+//   console.log('number ' + i)
+// }
+
+console.log(document.URL);
+
+
